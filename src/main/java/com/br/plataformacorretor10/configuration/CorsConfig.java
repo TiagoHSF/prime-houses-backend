c1cc008184eb1b1057885a6210a6c82fb38be27d
TiagoHSilva23 <tiago.ferreira@globalfinanceiro.com.br>
2023-11-14 09:15:02 -0300

    feat: Configuração de cors(INCOMPLETA)

---
+++ src/main/java/com/br/plataformacorretor10/configuration/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.br.plataformacorretor10.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * The Class CorsConfig
+ *
+ * @author Tiago Ferreira.
+ * @since 10/11/2023
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+
+}
